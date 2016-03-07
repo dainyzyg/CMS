5cbd1f8e561df7b295d705d2275b9561f0a6e57f
dainyzyg <470067721@qq.com>
2016-03-07 16:03:27 +0800

    修改配置文件

--- cms.config.js
+++ cms.config.js
@@ -3,9 +3,9 @@
  */
 var config = {
     mongodb: {
-        url: 'mongodb://localhost:27017/test',
-        userName: 'test1',
-        password: 'test1'
+        url: 'mongodb://11.11.185.36:27017/cms',
+        userName: 'cms',
+        password: 'cmspassword'
     }
 }
 
