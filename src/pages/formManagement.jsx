a16374950bb11d6dcfe1d8793a97fba859348c1a
michaellzg <lzshluzg@cnpc.com.cn>
2016-03-08 16:23:45 +0800

    新增表单管理界面

--- src/pages/formManagement.jsx
+++ src/pages/formManagement.jsx
@@ -1,25 +1,66 @@
-import ReactDOM from 'react-dom';
+﻿import ReactDOM from 'react-dom';
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Table, Icon } from 'antd';
 
-ReactDOM.render(
-    <div>
-        <Row>
-            <Col span="8">.test123</Col>
-            <Col span="8">.col-8</Col>
-            <Col span="8">.col-8</Col>
-        </Row>
-        <Row className="testRowClassName">
-            <Col span="8">.col-8</Col>
-            <Col span="8">.col-8</Col>
-            <Col span="8" className="testColClassName">.col-8</Col>
-        </Row>
-        <Row>
-            <Col span="6">.col-6</Col>
-            <Col span="6">.col-6</Col>
-            <Col span="6">.col-6</Col>
-            <Col span="6">.col-6</Col>
-        </Row>
-    </div>,
-    document.getElementById('react-content')
+const columns = [{
+    title: '表单ID',
+    dataIndex: 'formId',
+    render(text) {
+        return <a href="#">{text}</a>;
+    }
+}, {
+    title: '表单名称',
+    dataIndex: 'formName'
+}, {
+    title: '表单描述',
+    dataIndex: 'formDiscription'
+}, {
+    title: '创建时间',
+    dataIndex: 'formCreatetime'
+}, {
+    title: '最后修改时间',
+    dataIndex: 'formLastmodifytime'
+},
+    {
+        title: '操作',
+        dataIndex: 'formOperation',
+        render() {
+            return (
+                <span>
+        <a href="#">添加</a>
+        <span className="ant-divider"></span>
+        <a href="#">删除</a>
+        <span className="ant-divider"></span>
+            </span>
+            );
+        }
+    }
+];
+
+const data = [];
+for (let i = 1; i <= 30; i++) {
+    data.push({
+        key: i,
+        formId: `工作表单${i}号`,
+        formName: `信息化工作平台表单`,
+        formDiscription: `工作表单${i}号`,
+        formCreatetime: `20160308`,
+        formLastmodifytime: `20160308`,
+    });
+}
+
+const pagination = {
+    total: data.length,
+    current: 1,
+    showSizeChanger: true,
+    onShowSizeChange(current, pageSize) {
+        console.log('Current: ', current, '; PageSize: ', pageSize);
+    },
+    onChange(current) {
+        console.log('Current: ', current);
+    }
+};
+
+ReactDOM.render(<Table columns={columns} dataSource={data} pagination={pagination} bordered/>
+    , document.getElementById('react-content')
 );
