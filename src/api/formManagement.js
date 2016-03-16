60d584e43c421bbcdc0c9967be6f043f3050e5c0
baggio8688 <35408688@qq.com>
2016-03-16 16:45:55 +0800

    增加删除方法

--- src/api/formManagement.js
+++ src/api/formManagement.js
@@ -5,12 +5,16 @@ var path = require('path')
 var mongoDBHelper = require(path.resolve(process.cwd(), './src/lib/utils/mongoDBHelper'))
 
 exports.run = function(req, res) {
+    console.log(req.body.action)
     switch (req.body.action) {
         case 'save':
             save(req, res)
             break
         case 'getList':
             getList(req, res)
+            break
+        case 'deleteConfirm':
+            deleteConfirm(req, res)
             break
         default:
             break
@@ -27,12 +31,25 @@ function save(req, res) {
             res.send(JSON.stringify(result))
         })
 }
+//加载表单数据
 function getList(req, res) {
     var args = {
         index: parseInt(req.body.index),
         limit: parseInt(req.body.limit),
     }
     mongoDBHelper.runMongo('formManagement/getFormList', args,
+        (result) => {
+            console.log('result',JSON.stringify(result)),
+            res.send(JSON.stringify(result))
+        })
+}
+//删除数据
+function deleteConfirm(req, res) {
+    var args = {
+        _id: req.body._id
+    }
+    console.log('_id',args._id),
+    mongoDBHelper.runMongo('formManagement/deleteForm', args,
         (result) => {
             res.send(JSON.stringify(result))
         })
--- src/pages/formManagement.jsx
+++ src/pages/formManagement.jsx
@@ -1,15 +1,46 @@
 ﻿import ReactDOM from 'react-dom';
 import React from 'react';
-import { Table, Popconfirm } from 'antd';
+import { Table, Popconfirm, Modal, Alert} from 'antd';
 import Tablecomponent from './../lib/components/tableComponent.jsx';
+import reqwest from 'reqwest';
+//为删除确认定义方法
+const confirm = Modal.confirm;
+function deleteConfirm() {
+    console.log('id',this._id)
+    confirm({
+        title: '确认需要删除这项内容？',
+        onOk:()=> {
 
+            reqwest({
+                url: '../api/formManagement',
+                method: 'post',
+                data:{
+                    action: 'deleteConfirm',
+                    _id: this._id
+                },
+                type: 'json',
+                success: (result) => {
+                    //const pagination = this.state.pagination;
+                    //pagination.total = result.retval.totalCount;
+                    //this.setState({
+                    //    loading: false,
+                    //    data: result.retval.data,
+                    //    pagination,
+                    }})
+            console.log('确定');
+                },
+
+        onCancel() {
+        }
+    });
+}
 //为新增编辑器onClick事件定义方法
-function addEditor() {
-    window.location.href = '../dragform/dragform.html'
+function addEditor(){
+    window.location.href='../dragform/dragform.html'
 }
 //为编辑链接onClick事件定义方法
-function editContent() {
-    window.location.href = '../dragform/dragform.html'
+function editContent(){
+    window.location.href='../dragform/dragform.html'
 }
 //定义表头
 const columns = [{
@@ -25,11 +56,11 @@ const columns = [{
 }, {
     title: '创建时间',
     dataIndex: 'createTime',
-    width: 100
+    width:100
 }, {
     title: '最后修改时间',
     dataIndex: 'lastmodifyTime',
-    width: 100
+    width:100
 }, {
     title: '操作',
     dataIndex: 'formOperation',
@@ -39,6 +70,7 @@ const columns = [{
             <span>
                  <a href="#" onClick={editContent}>编辑</a>
                  <span className="ant-divider"></span>
+                 <a href="#" onClick={deleteConfirm.bind(record)}>删除</a>
                  <Popconfirm placement="left" title="确定要删除这条数据吗？">
                      <a href="#">删除</a>
                  </Popconfirm>
