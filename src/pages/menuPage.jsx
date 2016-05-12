4dff75e9dbb3ab0eb43a4f760f7f35deb59d3764
baggio8688 <35408688@qq.com>
2016-05-12 11:47:14 +0800

    -------------------------- 菜单管理 编辑数据绑定问题， key的问题， 分页问题， 新增无法保存， 排序问题 ------------------------- 用户管理 编辑后无法新增；

--- src/pages/menuPage.jsx
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Baggio on 2016/4/27. 菜单展示测试页面
- */
-import ReactDOM from 'react-dom';
-import React from 'react';
-import UserComponent from './../lib/components/views/menuPageComponent.jsx';
-
-ReactDOM.render(<UserComponent/>, document.getElementById('react-content'));
